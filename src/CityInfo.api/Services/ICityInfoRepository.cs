92d48ad8d629c082bb60cbce86789ec68c8908ee
Kokoc <kokolios1986@yahoo.gr>
2016-11-20 22:42:04 +0200

    working through repository

---
+++ src/CityInfo.api/Services/ICityInfoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityInfo.api.Controllers;
+using CityInfo.api.Entities;
+
+namespace CityInfo.api.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId,bool includePointsOfInterest);
+        IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId);
+        PointOfInterest GetPointOfInterestForCity(int cityId);
+
+    }
+}
